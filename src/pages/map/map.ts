f2e93749a7260c9bd6d001e0799a2b358890d0d5
Salvador Guzman <guzmansalv@gmail.com>
2017-10-05 18:51:11 -0700

    basic map.ts file

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,24 +1,36 @@
-import { Component } from '@angular/core';
-$IMPORTSTATEMENT
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {NavController, NavParams} from "ionic-angular";
 
-/**
- * Generated class for the Map page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-$IONICPAGE
+declare var google;
+
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
 })
 export class Map {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
+  loadMap(){
+    const latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+    const mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+  }
+
+  public ionViewDidLoad() {
     console.log('ionViewDidLoad Map');
+    this.loadMap();
   }
 
 }
